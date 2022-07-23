7c0e8b578f82dd0f6e5c884845b345a45808287c
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-07-22 23:20:44 -0500

    Ajustes al index

--- magic_trivia/apps/questions/views.py
+++ magic_trivia/apps/questions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import requests
+from django.views.generic import TemplateView
 # Create your views here.
+
+def get_category():
+    api_url = 'https://opentdb.com/api_category.php'
+    response = requests.get(api_url)
+    print(response.json())
+    return response.json()
+
+
+class home(TemplateView):
+    template_name = "index.html"
--- magic_trivia/apps/users/models.py
+++ magic_trivia/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    nick = models.CharField(max_length=100)
+    email = models.EmailField(max_length=120)
+    best_score = models.IntegerField(default=0)
+    num_matches = models.IntegerField(default=0)
+    password = models.CharField(max_length=50)
+
