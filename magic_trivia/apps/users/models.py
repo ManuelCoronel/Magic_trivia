0249790145fce107fb674df418651ddb4ebeab55
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-07-23 15:54:43 -0500

    ajustes al modelo de usuario heredado

--- magic_trivia/apps/users/models.py
+++ magic_trivia/apps/users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    nick = models.CharField(max_length=100)
-    email = models.EmailField(max_length=120)
+class CustomUser(AbstractUser):
     best_score = models.IntegerField(default=0)
     num_matches = models.IntegerField(default=0)
-    password = models.CharField(max_length=50)
+    
 
