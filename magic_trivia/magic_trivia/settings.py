9fa0b9221386147e6ee29fc410a1603eb1554769
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-08-23 09:39:36 -0500

    cambios menores en configuracion de variables de entorno

--- magic_trivia/magic_trivia/settings.py
+++ magic_trivia/magic_trivia/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w_nf2*3!@c3gc=owm&0gqnsid87yfc2(g2+#qc$yy#-@c7i12e'
+SECRET_KEY = os.environ["django-key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
