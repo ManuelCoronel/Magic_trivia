85fdcb2c4e872b853ccf67fb3d8c5861f57ae267
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-09-21 18:30:11 -0500

    Path principal agregado

--- magic_trivia/magic_trivia/urls.py
+++ magic_trivia/magic_trivia/urls.py
@@ -3,8 +3,11 @@
 from django.contrib import admin
 from django.urls import path,include
 
+
+main_path = 'magic/'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('apps.questions.urls')),
-    path('account/',include('apps.users.urls')),
+    path(main_path+'admin/', admin.site.urls),
+    path(main_path+'',include('apps.questions.urls')),
+    path(main_path+'account/',include('apps.users.urls')),
 ]
