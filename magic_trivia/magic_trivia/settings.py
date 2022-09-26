f0ff542bd8c564f91afbf7c2f6dffaff576c81a0
ManuelCoronel <coronel.andrade.manuel@gmail.com>
2022-09-25 21:02:23 -0500

    static url con contexto

--- magic_trivia/magic_trivia/settings.py
+++ magic_trivia/magic_trivia/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/magic/static/'
+STATIC_URL =  '/magic/static/'
+STATIC_ROOT = '/static/'
 
 
 
